f8853d2e8cf599779d69755bcc9ecc7995532955
Hyunsu Joo <hs9880@gmail.com>
2020-11-11 16:38:13 +0900

    Add basic functional  mini project

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI:"mongodb://hyunsu_9880:Algorithm^910@cluster0-shard-00-00.zokyl.mongodb.net:27017,cluster0-shard-00-01.zokyl.mongodb.net:27017,cluster0-shard-00-02.zokyl.mongodb.net:27017/bloggingPlatform?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: '123123123',
+};
